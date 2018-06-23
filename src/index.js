41c0ec1c9f19533b2bc41cc9eb7e53a73e7bff18
Joshua Levine <joshuapark95@gmail.com>
2018-06-23 15:54:04 -0500

    Import abis & addresses

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
-import "./index.css";
 import App from "./App";
 import configureStore from "./store";
 
