3d98132036241aac8f581a54d88be78a43bd40a7
Joshua Levine <joshuapark95@gmail.com>
2018-09-02 22:49:40 -0700

    better modal scrolling & set cold active after link is approved

--- src/_mock/topics.js
+++ src/_mock/topics.js
@@ -67,7 +67,7 @@ export default {
       topic_blurb: `Here you can vote YES or NO to the proposed five core principles of the Maker Governance philosophy as <a target="_blank" rel="noopener noreferrer" href="https://medium.com/makerdao/foundation-proposal-v2-f10d8ee5fe8c"> updated on Medium</a> on August 21, 2018
       This type of vote is a <strong style="font-weight:bold;font-style:oblique;">Governance Vote</strong>, and its objective is to represent resolution on a matter or collection of matters. This vote will not result in changes to the Dai Credit System (to change the state of the system, for example to change the debt ceiling, a second type of vote called an <strong style="font-weight:bold;font-style:oblique;">Executive Vote</strong> is used).`,
       end_timestamp: 1537027200000,
-      date: '2018-03-12', //yyyy-mm-dd
+      date: 1536768000000, //yyyy-mm-dd
       verified: true,
       submitted_by: {
         name: 'Dai Foundation',
@@ -81,7 +81,7 @@ export default {
           about: require('./yes-proposal.md'),
           source: '0x0c0fC0952790A96D60CD82cA865C7bb1233477C3',
           end_timestamp: 1537027200000,
-          date: '2018-03-12', //yyyy-mm-dd
+          date: 1536768000000, //yyyy-mm-dd
           verified: true,
           submitted_by: {
             name: 'Dai Foundation',
@@ -101,7 +101,7 @@ export default {
           about: require('./no-proposal.md'),
           source: '0x66fF2801fDfEa24db2873abf977cA32226c175BD',
           end_timestamp: 1537027200000,
-          date: '2018-03-12',
+          date: 1536768000000,
           verified: true,
           submitted_by: {
             name: 'Dai Foundation',
