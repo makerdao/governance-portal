97162b630abf9d01d8f61611614be653ad67ec99
David Terry <me@xwvvvvwx.com>
2018-09-04 18:55:49 +0200

    topics: test kovan also, fix bug in mocked kovan data

--- test/reducers/topics.test.js
+++ test/reducers/topics.test.js
@@ -1,50 +1,54 @@
 import { topicsInit } from '../../src/reducers/topics';
+import each from 'jest-each';
 
 const dateRegex = '^\\d{4}-\\d{2}-\\d{2}$';
 
-test('topicsInit returns a TOPICS_SUCCESS action with topics as a payload', async () => {
-  const dispatch = jest.fn();
-  await topicsInit('mainnet')(dispatch);
+each(['mainnet', 'kovan']).test(
+  'topicsInit returns a TOPICS_SUCCESS action with topics as a payload',
+  async network => {
+    const dispatch = jest.fn();
+    await topicsInit(network)(dispatch);
 
-  expect(dispatch.mock.calls.length).toBe(2);
-  expect(dispatch.mock.calls[0][0]).toEqual({
-    type: 'topics/TOPICS_SUCCESS',
-    payload: expect.arrayContaining([
-      {
-        topic: expect.any(String),
-        id: expect.any(Number),
-        active: expect.any(Boolean),
-        govVote: expect.any(Boolean),
-        topic_blurb: expect.any(String),
-        end_timestamp: expect.any(Number),
-        date: expect.stringMatching(dateRegex),
-        verified: expect.any(Boolean),
-        submitted_by: {
-          name: expect.any(String),
-          link: expect.any(String)
-        },
-        proposals: expect.arrayContaining([
-          {
-            title: expect.any(String),
-            proposal_blurb: expect.any(String),
-            about: expect.any(String),
-            source: expect.any(String),
-            end_timestamp: expect.any(Number),
-            date: expect.stringMatching(dateRegex),
-            verified: expect.any(Boolean),
-            submitted_by: {
-              name: expect.any(String),
-              link: expect.any(String)
-            },
-            documents: expect.arrayContaining([
-              {
+    expect(dispatch.mock.calls.length).toBe(2);
+    expect(dispatch.mock.calls[0][0]).toEqual({
+      type: 'topics/TOPICS_SUCCESS',
+      payload: expect.arrayContaining([
+        {
+          topic: expect.any(String),
+          id: expect.any(Number),
+          active: expect.any(Boolean),
+          govVote: expect.any(Boolean),
+          topic_blurb: expect.any(String),
+          end_timestamp: expect.any(Number),
+          date: expect.stringMatching(dateRegex),
+          verified: expect.any(Boolean),
+          submitted_by: {
+            name: expect.any(String),
+            link: expect.any(String)
+          },
+          proposals: expect.arrayContaining([
+            {
+              title: expect.any(String),
+              proposal_blurb: expect.any(String),
+              about: expect.any(String),
+              source: expect.any(String),
+              end_timestamp: expect.any(Number),
+              date: expect.stringMatching(dateRegex),
+              verified: expect.any(Boolean),
+              submitted_by: {
                 name: expect.any(String),
                 link: expect.any(String)
-              }
-            ])
-          }
-        ])
-      }
-    ])
-  });
-});
+              },
+              documents: expect.arrayContaining([
+                {
+                  name: expect.any(String),
+                  link: expect.any(String)
+                }
+              ])
+            }
+          ])
+        }
+      ])
+    });
+  }
+);
