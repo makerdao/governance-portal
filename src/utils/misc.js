e1a1ad1cd8428b3a8600833b4bb583d89220bc89
Tyler Sorensen <tyler@makerdao.com>
2018-09-11 19:52:32 -0700

    countDecimals can handle small numbers

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -192,7 +192,7 @@ export const promisedProperties = obj => {
  * @return {Promise}
  */
 export const countDecimals = _value => {
-  const value = Number(_value);
+  const value = Number(_value).toFixed(18);
   if (value % 1 !== 0) return value.toString().split('.')[1].length;
   return 0;
 };
