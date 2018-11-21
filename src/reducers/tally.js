9f7243ad5cf1d396b6a5fa8c55a0ec4181d8f0fd
Phil Bain <bain.pmcg@gmail.com>
2018-11-21 21:55:01 +0100

    refactor tally.js to use the gov plugin

--- src/reducers/tally.js
+++ src/reducers/tally.js
@@ -1,30 +1,28 @@
 import { createReducer } from '../utils/redux';
-// import { promiseRetry } from '../utils/misc';
+import { promiseRetry } from '../utils/misc';
 
 // Constants ----------------------------------------------
 
-const TALLY_REQUEST = 'voteTally/TALLY_REQUEST';
-const TALLY_SUCCESS = 'voteTally/TALLY_SUCCESS';
-const TALLY_FAILURE = 'voteTally/TALLY_FAILURE';
+export const TALLY_REQUEST = 'voteTally/TALLY_REQUEST';
+export const TALLY_SUCCESS = 'voteTally/TALLY_SUCCESS';
+export const TALLY_FAILURE = 'voteTally/TALLY_FAILURE';
 export const TALLY_UPDATE = 'voteTally/TALLY_UPDATE';
 
 // Actions ------------------------------------------------
 
 export const voteTallyInit = () => dispatch => {
+  const getVoteTally = window.maker.service('chief').getVoteTally();
   dispatch({ type: TALLY_REQUEST });
-  // promiseRetry({ times: 3, fn: fakeCb, delay: 500 })
-  const tally = 5;
-  dispatch({ type: TALLY_SUCCESS, payload: { tally } });
-
-  // .then(tally => {
-  //   dispatch({ type: TALLY_SUCCESS, payload: { tally } });
-  // })
-  // // sometimes this fails when we're reading event logs
-  // .catch(error => {
-  //   // TODO: notify user or throw to a fallback component
-  //   console.error(error);
-  //   dispatch({ type: TALLY_FAILURE });
-  // });
+  promiseRetry({ times: 3, fn: getVoteTally, delay: 500 })
+    .then(tally => {
+      dispatch({ type: TALLY_SUCCESS, payload: { tally } });
+    })
+    // sometimes this fails when we're reading event logs
+    .catch(error => {
+      // TODO: notify user or throw to a fallback component
+      console.error(error);
+      dispatch({ type: TALLY_FAILURE });
+    });
 };
 
 // Reducer ------------------------------------------------
