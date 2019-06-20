e530f4153b2bf05725120041c6357e9e0086275a
shkfnly <ashoka.finley@gmail.com>
2019-06-20 16:34:40 -0700

    update hashing library

--- src/utils/ipfs.js
+++ src/utils/ipfs.js
@@ -1,15 +1,9 @@
-const Unixfs = require('ipfs-unixfs');
-const { DAGNode } = require('ipld-dag-pb');
+const Hash = require('ipfs-only-hash');
 
 export const generateIPFSHash = async (data, options) => {
   // options object has the key encoding which defines the encoding type
   // of the data string that has been passed in
   const bufferData = Buffer.from(data, options.encoding || 'ascii');
-  const unixFs = new Unixfs('file', bufferData);
-
-  let result = await DAGNode.create(unixFs.marshal(), (err, dagNode) => {
-    if (err) return console.error(err);
-    return dagNode.toJSON().multihash;
-  });
-  return result;
+  const hash = await Hash.of(bufferData);
+  return hash;
 };
