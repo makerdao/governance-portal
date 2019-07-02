a5d247fa4d56753d98be818f6285e75472af2878
sirromdev <sirromdev@gmail.com>
2019-07-02 18:15:57 +0100

    added UX to form

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -112,6 +112,9 @@ const DEFAULT_START = new Date();
 const DEFAULT_END = new Date(DEFAULT_START.getTime() + 7 * 24 * 60 * 60 * 1000);
 const MIN_POLL_DURATION = 24 * 60 * 60 * 1000;
 
+const expr = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+const URL_REGEX = new RegExp(expr);
+
 function calculateTimeSpan(earlier, later) {
   let timeSpanInSeconds = Math.abs(earlier.getTime() - later.getTime()) / 1000;
   let span = {};
@@ -258,6 +261,7 @@ class Admin extends Component {
     const isValidSubmission =
       !!poll.title &&
       !!poll.summary &&
+      poll.link.match(URL_REGEX) &&
       poll.choices.length > 2 &&
       !!poll.content &&
       this.pollTimeWindowIsValid();
@@ -309,27 +313,31 @@ class Admin extends Component {
               <SectionWrapper>
                 <StyledBody>Title:</StyledBody>
                 <Input
-                  width="400px"
+                  width="600px"
                   placeholder="This will be the poll title"
                   value={poll.title}
                   onChange={e => this.handlePollState(e, 'title')}
+                  success={poll.title.length > 0}
+                  error={poll.title.length === 0}
                 />
               </SectionWrapper>
 
               <SectionWrapper>
                 <StyledBody>Summary:</StyledBody>
                 <Input
-                  width="400px"
+                  width="600px"
                   placeholder="Give a short description of what this Poll is for"
                   value={poll.summary}
                   onChange={e => this.handlePollState(e, 'summary')}
+                  success={poll.summary.length > 0}
+                  error={poll.summary.length === 0}
                 />
               </SectionWrapper>
 
               <SectionWrapper>
                 <StyledBody>Poll Start Time:</StyledBody>
                 <DateTimePicker
-                  css={{ width: '400px' }}
+                  css={{ width: '600px' }}
                   disableClock
                   showLeadingZeros
                   clearIcon
@@ -341,7 +349,7 @@ class Admin extends Component {
               <SectionWrapper>
                 <StyledBody>Poll End Time:</StyledBody>
                 <DateTimePicker
-                  css={{ width: '400px' }}
+                  css={{ width: '600px' }}
                   disableClock
                   showLeadingZeros
                   clearIcon
@@ -352,25 +360,40 @@ class Admin extends Component {
 
               <SectionWrapper>
                 <StyledBody>Poll Duration</StyledBody>
-                <StyledBody css={{ width: '400px' }}>
-                  {calculateTimeSpan(poll.start, poll.end)}
-                </StyledBody>
+                <Input
+                  width="600px"
+                  disabled
+                  value={calculateTimeSpan(poll.start, poll.end)}
+                  success={this.pollTimeWindowIsValid()}
+                  error={!this.pollTimeWindowIsValid()}
+                  failureMessage={
+                    !this.pollTimeWindowIsValid() &&
+                    'Poll must have a duration of at least 1 day'
+                  }
+                />
               </SectionWrapper>
 
               <SectionWrapper>
                 <StyledBody>Discussion Link:</StyledBody>
                 <Input
-                  width="400px"
+                  width="600px"
                   placeholder="Link to where this Polling proposal will be discussed"
                   value={poll.link}
                   onChange={e => this.handlePollState(e, 'link')}
+                  success={poll.link.match(URL_REGEX)}
+                  error={!poll.link.match(URL_REGEX)}
+                  failureMessage={
+                    poll.link !== '' &&
+                    !poll.link.match(URL_REGEX) &&
+                    'This must be a url'
+                  }
                 />
               </SectionWrapper>
 
               <SectionWrapper>
                 <StyledBody>Poll Rules:</StyledBody>
                 <Input
-                  width="400px"
+                  width="600px"
                   placeholder="Indicate how the Poll is to be conducted"
                   value={poll.rules}
                   onChange={e => this.handlePollState(e, 'rules')}
@@ -385,25 +408,35 @@ class Admin extends Component {
                   placeholder="Write (in markdown) the full polling proposal"
                   value={poll.content}
                   onChange={e => this.handlePollState(e, 'content')}
+                  success={poll.content}
+                  error={!poll.content}
                 />
               </SectionWrapper>
 
               <SectionWrapper>
                 <StyledBody>Vote Options:</StyledBody>
                 <Input
-                  width="400px"
+                  width="600px"
                   placeholder="Add possible voting options"
                   value={poll.option}
                   onChange={e => this.handlePollState(e, 'option')}
                   maxLength={25}
+                  sucess={poll.choices.length > 2}
+                  error={poll.choices.length <= 2}
+                  failureMessage={
+                    poll.choices.length <= 2 &&
+                    'Must be at least three voting options'
+                  }
+                  after={
+                    <Button
+                      css={{ alignSelf: 'center', marginLeft: '10px' }}
+                      width="190px"
+                      onClick={this.addPollVoteOption}
+                    >
+                      Add Option
+                    </Button>
+                  }
                 />
-                <Button
-                  css={{ alignSelf: 'center', marginLeft: '10px' }}
-                  width="190px"
-                  onClick={this.addPollVoteOption}
-                >
-                  Add Option
-                </Button>
               </SectionWrapper>
 
               <SectionWrapper>
