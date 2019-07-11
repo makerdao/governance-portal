cac9d1b46decbed8cae63997d2f65c6a81255d88
sirromdev <sirromdev@gmail.com>
2019-07-11 16:17:16 +0100

    user can only exec createPoll if account exists

--- src/components/CreatePoll/CreatePollOverview.js
+++ src/components/CreatePoll/CreatePollOverview.js
@@ -1,10 +1,12 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
 import { Box } from '@makerdao/ui-components-core';
 import { Button } from '@makerdao/ui-components';
 import { copyToClipboard } from '../../utils/misc';
 import { URL_REGEX } from '../../utils/constants';
 import CreatePollInput from './CreatePollInput';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 const StyledBody = styled.p`
   width: 150px;
@@ -37,111 +39,143 @@ const Code = styled.pre`
   white-space: pre-wrap;
 `;
 
-export default function CreatePollOverview({
-  start,
-  end,
-  markdown,
-  hash,
-  url,
-  submitAttempted,
-  handleParentState,
-  execCreatePoll
-}) {
-  const urlValid = url.match(URL_REGEX);
-  const urlError = submitAttempted && !urlValid;
+const WarningText = styled.p`
+  font-size: 0.9em;
+  color: #f35833;
+  margin-top: 11px;
+`;
 
-  const handlePollState = (e, key) => {
-    e.stopPropagation();
-    handleParentState({
-      [key]: e.target.value
-    });
-  };
+class CreatePollOverview extends Component {
+  render = () => {
+    const {
+      start,
+      end,
+      markdown,
+      hash,
+      url,
+      submitAttempted,
+      handleParentState,
+      execCreatePoll,
+      allAccounts
+    } = this.props;
+    const urlValid = url.match(URL_REGEX);
+    const urlError = submitAttempted && !urlValid;
 
-  return (
-    <Fragment>
-      <SectionText>
-        This is an overview of the new poll. The polling window will be open
-        from the {start.toUTCString()} and will close on {end.toUTCString()}.
-      </SectionText>
-      <SectionText>
-        The markdown and hash below should be copied into the cms and a the
-        subsequent poll's content url should be retrieved and pasted in the
-        input field below.
-      </SectionText>
-      <SectionWrapper>
-        <Box>
-          <StyledBody>Markdown:</StyledBody>
-          <Button
-            css={{ marginTop: '10px' }}
-            variant="secondary"
+    const execPollValid = allAccounts.length;
+    const execPollError = submitAttempted && !execPollValid;
+
+    const handlePollState = (e, key) => {
+      e.stopPropagation();
+      handleParentState({
+        [key]: e.target.value
+      });
+    };
+
+    return (
+      <Fragment>
+        <SectionText>
+          This is an overview of the new poll. The polling window will be open
+          from the {start.toUTCString()} and will close on {end.toUTCString()}.
+        </SectionText>
+        <SectionText>
+          The markdown and hash below should be copied into the cms and a the
+          subsequent poll's content url should be retrieved and pasted in the
+          input field below.
+        </SectionText>
+        <SectionWrapper>
+          <Box>
+            <StyledBody>Markdown:</StyledBody>
+            <Button
+              css={{ marginTop: '10px' }}
+              variant="secondary"
+              onClick={() => copyToClipboard(markdown)}
+            >
+              Copy
+            </Button>
+          </Box>
+          <Code
+            css={{
+              width: '800px',
+              overflow: 'auto',
+              cursor: 'pointer'
+            }}
             onClick={() => copyToClipboard(markdown)}
+          >
+            {markdown}
+          </Code>
+        </SectionWrapper>
+
+        <SectionWrapper>
+          <StyledBody>Hash:</StyledBody>
+          <SectionText
+            css={{
+              width: '600px',
+              cursor: 'pointer'
+            }}
+            onClick={() => copyToClipboard(hash)}
+          >
+            {hash}
+          </SectionText>
+          <Button
+            variant="secondary"
+            onClick={() => copyToClipboard(hash)}
+            css={{ width: '200px' }}
           >
             Copy
           </Button>
-        </Box>
-        <Code
-          css={{
-            width: '800px',
-            overflow: 'auto',
-            cursor: 'pointer'
-          }}
-          onClick={() => copyToClipboard(markdown)}
-        >
-          {markdown}
-        </Code>
-      </SectionWrapper>
+        </SectionWrapper>
 
-      <SectionWrapper>
-        <StyledBody>Hash:</StyledBody>
-        <SectionText
-          css={{
-            width: '600px',
-            cursor: 'pointer'
+        <CreatePollInput
+          {...{
+            title: 'Poll URL',
+            placeholder: 'The URL from which the poll can be found',
+            value: url,
+            onChange: e => handlePollState(e, 'url'),
+            error: urlError,
+            failureMessage: urlError && 'The Poll URL must be a valid url',
+            width: '800px'
           }}
-          onClick={() => copyToClipboard(hash)}
-        >
-          {hash}
-        </SectionText>
-        <Button
-          variant="secondary"
-          onClick={() => copyToClipboard(hash)}
-          css={{ width: '200px' }}
-        >
-          Copy
-        </Button>
-      </SectionWrapper>
+        />
 
-      <CreatePollInput
-        {...{
-          title: 'Poll URL',
-          placeholder: 'The URL from which the poll can be found',
-          value: url,
-          onChange: e => handlePollState(e, 'url'),
-          error: urlError,
-          failureMessage: urlError && 'The Poll URL must be a valid url',
-          width: '800px'
-        }}
-      />
+        <SectionWrapper css={{ marginTop: '20px' }}>
+          <Button
+            onClick={async () => {
+              handleParentState({ submitAttempted: true });
+              if (urlValid && execPollValid) {
+                execCreatePoll();
+              }
+            }}
+            disabled={execPollError}
+          >
+            Create Poll
+          </Button>
+          <Box width="32px" />
+          <Button
+            variant="secondary"
+            onClick={() => handleParentState({ step: 0 })}
+          >
+            Edit
+          </Button>
+        </SectionWrapper>
 
-      <SectionWrapper css={{ marginTop: '20px' }}>
-        <Button
-          onClick={() => {
-            handleParentState({ submitAttempted: true });
-            if (urlValid) {
-              execCreatePoll();
-            }
-          }}
-        >
-          Create Poll
-        </Button>
-        <Box width="32px" />
-        <Button
-          variant="secondary"
-          onClick={() => handleParentState({ step: 0 })}
-        >
-          Edit
-        </Button>
-      </SectionWrapper>
-    </Fragment>
-  );
+        {execPollError && (
+          <SectionWrapper>
+            <WarningText>
+              User must have an account to create the poll
+            </WarningText>
+          </SectionWrapper>
+        )}
+      </Fragment>
+    );
+  };
 }
+
+CreatePollOverview.propTypes = {
+  allAccounts: PropTypes.array
+};
+
+const mapStateToProps = ({ accounts }, props) => ({
+  allAccounts: accounts.allAccounts
+});
+
+export default connect(mapStateToProps)(CreatePollOverview);
