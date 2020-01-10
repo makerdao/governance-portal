c147bf70396a17b8b92579d903c99b51f63dbf08
shkfnly <ashoka.finley@gmail.com>
2020-01-09 21:00:36 -0500

    Start Emergency Shutdown Module

--- src/pages/Modules.js
+++ src/pages/Modules.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
+import { Flex, Grid, Text } from '@makerdao/ui-components-core';
 
 import Loader from '../components/Loader';
-
-const Container = styled.div`
-  padding: 50px;
-`;
 
 export default class Dropdown extends React.Component {
   state = {
@@ -22,41 +19,11 @@ export default class Dropdown extends React.Component {
   render() {
     const { esmCliDocContent } = this.state;
     return (
-      <Container>
-        {esmCliDocContent ? (
-          <div>
-            <p>
-              Emergency Shutdown is currently only available through the command
-              line interface as we are in the process of building a frontend UI
-              for users to interact with. This guide therefore outlines the
-              steps and procedures necessary to check, interact with and trigger
-              the ESM.
-            </p>
-            <br />
-            <p>
-              The Emergency Shutdown Module (ESM) is responsible for a process
-              that gracefully shuts down the Maker Protocol and properly
-              allocates collateral to both Vault users and Dai holders. This
-              acts as a last resort to protect the Maker Protocol against a
-              serious threat, such as but not limited to governance attacks,
-              long-term market irrationality, hacks and security breaches.
-            </p>
-            <br />
-            <p>
-              Please be aware that the triggering of the ESM is not to be taken
-              lightly as this action permanently burns the users MKR tokens.
-            </p>
-            <br />
-            <ReactMarkdown
-              className="markdown"
-              skipHtml={false}
-              source={esmCliDocContent}
-            />
-          </div>
-        ) : (
-          <Loader mt={34} mb={34} color="header" background="background" />
-        )}
-      </Container>
+      <Flex flexDirection="column" minHeight="100vh">
+        <Grid gridRowGap="m" mx={'2xl'} my={'2xl'} px={'2xl'}>
+          <Text.h2 textAlign="left">Emergency Shutdown Module</Text.h2>
+        </Grid>
+      </Flex>
     );
   }
 }
