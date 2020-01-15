58cfa72160ed56426bdde2e59e23c30970874c25
Josh Levine <joshuapark95@gmail.com>
2020-01-15 02:22:35 -0500

    rmv unused imports + make esm component a funciton

--- src/pages/Modules.js
+++ src/pages/Modules.js
@@ -1,64 +1,41 @@
-import React, { useState } from 'react';
-import {
-  Box,
-  Button,
-  Card,
-  CardBody,
-  Flex,
-  Grid,
-  Text,
-  Link
-} from '@makerdao/ui-components-core';
+import React from 'react';
+import { Flex, Grid, Text, Link } from '@makerdao/ui-components-core';
 import { connect } from 'react-redux';
 import { getActiveAccount } from '../reducers/accounts';
-import { MKR } from '../chain/maker';
 
 import MKRBurn from '../components/Modules/MKRBurn';
 import ESMHistory from '../components/Modules/ESMHistory';
 
-class ESM extends React.Component {
-  state = {
-    esmCliDocContent: null
-  };
+const ESM = ({ activeAccount = {} } = {}) => {
+  const { mkrInEsm } = activeAccount;
 
-  componentDidMount() {
-    fetch(require('../esCliDoc.md'))
-      .then(res => res.text())
-      .then(esmCliDocContent => this.setState({ esmCliDocContent }));
-  }
-
-  render() {
-    const { mkrInEsm } = this.props.activeAccount;
-    const { esmCliDocContent } = this.state;
-
-    return (
-      <Flex flexDirection="column" minHeight="100vh">
-        <Grid gridRowGap="m" mx={'2xl'} my={'2xl'} px={'2xl'}>
-          <Text.h2 textAlign="left">Emergency Shutdown Module</Text.h2>
-          <Text.p textAlign="justify">
-            The ESM allows MKR holders to shutdown the system without a central
-            authority. Once 50,000 MKR are entered into the ESM, emergency
-            shutdown can be executed.
-            {` `}
-            <Link
-              target="_blank"
-              rel="noopener noreferrer"
-              css="text-decoration: none"
-            >
-              Read the documentation here.
-            </Link>
-          </Text.p>
-          <MKRBurn mkrInEsm={mkrInEsm} />
-          <ESMHistory />
-        </Grid>
-      </Flex>
-    );
-  }
-}
+  return (
+    <Flex flexDirection="column" minHeight="100vh">
+      <Grid gridRowGap="m" mx={'2xl'} my={'2xl'} px={'2xl'}>
+        <Text.h2 textAlign="left">Emergency Shutdown Module</Text.h2>
+        <Text.p textAlign="justify">
+          The ESM allows MKR holders to shutdown the system without a central
+          authority. Once 50,000 MKR are entered into the ESM, emergency
+          shutdown can be executed.
+          {` `}
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            css="text-decoration: none"
+          >
+            Read the documentation here.
+          </Link>
+        </Text.p>
+        <MKRBurn mkrInEsm={mkrInEsm} />
+        <ESMHistory />
+      </Grid>
+    </Flex>
+  );
+};
 
 const reduxProps = ({ accounts }) => {
   return {
-    activeAccount: getActiveAccount({ accounts }) || {}
+    activeAccount: getActiveAccount({ accounts })
   };
 };
 
