83ed2372ad627d8128925a2fe772987b1f0d66ad
Lawrence Wang <l@lw.io>
2020-02-03 18:20:34 -0800

    initial commit of circleci config

---
+++ scripts/now-deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+cat > ./src/static/version.json <<- EOM
+{
+  "COMMIT_SHA": "${CIRCLE_SHA1}",
+  "COMMIT_BRANCH": "${CIRCLE_BRANCH}"
+}
+EOM
+
+cat ./src/static/version.json
+
+NOW_EXEC=./node_modules/.bin/now
+FROM_NAME=`$NOW_EXEC --token $NOW_TOKEN --scope mkr-js-prod`
+BRANCH=`echo ${CIRCLE_BRANCH//./-} | tr '[:upper:]' '[:lower:]'`
+TO_NAME=governance-dashboard-git-${BRANCH}.mkr-js-prod.now.sh
+
+$NOW_EXEC alias --token $NOW_TOKEN --scope mkr-js-prod $FROM_NAME $TO_NAME
