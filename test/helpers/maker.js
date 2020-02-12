29ac4bd74352ea3c2259562eceeb03f3bbd7b6a0
shkfnly <ashoka.finley@gmail.com>
2020-02-12 17:13:58 -0500

    fix call exception

--- test/helpers/maker.js
+++ test/helpers/maker.js
@@ -1,4 +1,4 @@
-import Maker from '@makerdao/dai';
+import Maker, { MKR } from '@makerdao/dai';
 import governancePlugin from '@makerdao/dai-plugin-governance';
 import trezorPlugin from '@makerdao/dai-plugin-trezor-web';
 import ledgerPlugin from '@makerdao/dai-plugin-ledger-web';
@@ -21,6 +21,23 @@ export default async function instantiateMaker(network) {
     plugins: [trezorPlugin, ledgerPlugin, governancePlugin],
     autoAuthenticate: true
   };
+
+  const token = {
+    erc20: [
+      {
+        currency: MKR,
+        symbol: MKR.symbol,
+        address: '0x1c3ac7216250edc5b9daa5598da0579688b9dbd5'
+      }
+      // {
+      //   currency: IOU,
+      //   symbol: IOU.symbol,
+      //   address: kovanMcdAddresses.IOU
+      // }
+    ]
+  };
+
+  config.token = token;
 
   let maker = await Maker.create('http', config);
   window.maker = maker; // for debugging
