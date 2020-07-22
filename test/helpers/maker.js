ee140cb12d61c7bf66d336f4aec81da6fe88af00
Josh Levine <24902242+jparklev@users.noreply.github.com>
2020-07-22 12:16:20 -0400

    Revert "switch from infura to alchemy, get api key from environment"

--- test/helpers/maker.js
+++ test/helpers/maker.js
@@ -1,11 +1,14 @@
 import Maker from '@makerdao/dai';
 import governancePlugin from '@makerdao/dai-plugin-governance';
+import trezorPlugin from '@makerdao/dai-plugin-trezor-web';
 import ledgerPlugin from '@makerdao/dai-plugin-ledger-web';
-import { netToUri } from '../../src/utils/ethereum';
+const INFURA_KEY = '6ba7a95268bf4ccda9bf1373fe582b43';
 
 export default async function instantiateMaker(network) {
   const url =
-    network === 'testnet' ? process.env.TEST_RPC_URL : netToUri(network);
+    network === 'testnet'
+      ? process.env.TEST_RPC_URL
+      : `https://${network}.infura.io/v3/${INFURA_KEY}`;
 
   // this is required here instead of being imported normally because it runs
   // code that will break if run server-side
