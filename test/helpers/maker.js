fd454db56d56f21535b69bf79b0d34b7c6a5dc2f
Josh Levine <joshuapark95@gmail.com>
2020-11-17 12:18:21 -0500

    Revert "infura -> alchemy"
    This reverts commit 3ac5700b364191b0e204e657eb177d8d4c592d02.

--- test/helpers/maker.js
+++ test/helpers/maker.js
@@ -2,13 +2,13 @@ import Maker from '@makerdao/dai';
 import governancePlugin from '@makerdao/dai-plugin-governance';
 import trezorPlugin from '@makerdao/dai-plugin-trezor-web';
 import ledgerPlugin from '@makerdao/dai-plugin-ledger-web';
-const ALCHEMY_KEY = 'Z8mVj3Mv-Ejd-S97690_uHhlg8nZmvHl';
+const INFURA_KEY = '6ba7a95268bf4ccda9bf1373fe582b43';
 
 export default async function instantiateMaker(network) {
   const url =
     network === 'testnet'
       ? process.env.TEST_RPC_URL
-      : `https://eth-${network}.alchemyapi.io/v2/${ALCHEMY_KEY}`;
+      : `https://${network}.infura.io/v3/${INFURA_KEY}`;
 
   // this is required here instead of being imported normally because it runs
   // code that will break if run server-side
