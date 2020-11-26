3251f2255d275ba4adda3e1c24c1175d2011f987
Lawrence Wang <l@lw.io>
2020-11-25 17:03:19 -0800

    remove unused maker config

--- src/chain/maker.js
+++ src/chain/maker.js
@@ -3,13 +3,11 @@ import trezorPlugin from '@makerdao/dai-plugin-trezor-web';
 import ledgerPlugin from '@makerdao/dai-plugin-ledger-web';
 import Maker, { ETH, MKR } from '@makerdao/dai';
 import configPlugin from '@makerdao/dai-plugin-config';
-import { createCurrency } from '@makerdao/currency';
 
 import { netToUri } from '../utils/ethereum';
 
 export default async function createMaker(
   network = 'mainnet',
-  useMcdKovanContracts,
   testchainConfigId,
   backendEnv
 ) {
@@ -32,37 +30,6 @@ export default async function createMaker(
         VOTE_PROXY_FACTORY: '0x1400798AA746457E467A1eb9b3F3f72C25314429'
       }
     };
-  }
-
-  if (useMcdKovanContracts) {
-    const MKR = createCurrency('MKR');
-    const IOU = createCurrency('IOU');
-    const kovanMcdAddresses = require('./addresses/kovan-mcd.json');
-    const addContracts = Object.keys(kovanMcdAddresses).reduce(
-      (result, key) => {
-        result[key] = { address: { kovan: kovanMcdAddresses[key] } };
-        return result;
-      },
-      {}
-    );
-
-    const token = {
-      erc20: [
-        {
-          currency: MKR,
-          symbol: MKR.symbol,
-          address: kovanMcdAddresses.GOV
-        },
-        {
-          currency: IOU,
-          symbol: IOU.symbol,
-          address: kovanMcdAddresses.IOU
-        }
-      ]
-    };
-
-    config.smartContract = { addContracts };
-    config.token = token;
   }
 
   // Use the config plugin, if we have a testchainConfigId
